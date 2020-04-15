6524eae7657c027830f40723300432044759ef33
AeBliznyuk <bliznyuksaha@mail.ru>
2020-04-15 13:56:21 +0700

    Добавьте файлы проекта.

---
+++ NoteApp/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    public enum Category
+    {
+      Job,
+      Home,
+      Health_and_Sportт,
+      People,
+      Documents,
+      Finance,
+      Other
+    }
+}
---
+++ NoteApp/Note.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{   
+    class Note
+    {
+        private string _name = "Без названия";
+        
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+                
+            }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException("Длина текста больше 50 символов");
+                }
+                else _name = value;
+            }
+        }
+        public Category Category { get; set; }
+      
+
+        public DateTime CreateDateTime { get; private set; }
+        public DateTime ChangeDateTime { get; private set; }
+
+    }
+}
+
