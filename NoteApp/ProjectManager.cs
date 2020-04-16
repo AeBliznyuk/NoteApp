1baa04cf5b546dc850d73e4197668c449fa47975
AeBliznyuk <bliznyuksaha@mail.ru>
2020-04-16 14:15:17 +0700

    feature(ProjectManager): Созданы методы LoadFromFile и SaveToFile.

--- NoteApp/ProjectManager.cs
+++ NoteApp/ProjectManager.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System.IO;
+
 
 namespace NoteApp
 {
-    public class ProjectManager
+    static public class ProjectManager
     {
+        static public Project LoadFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                Project project = null;
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamReader sr = new StreamReader(@"c:\json.txt"))
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    project = (Project)serializer.Deserialize<Project>(reader);
+                }
+                return project;
+            }
+            else return null;
+        }
+        static public void SaveToFile(string path, Project project)
+        {
+            if (!File.Exists(path))
+            {
+                using (FileStream fileStream = File.Create(path)) { }
+            }
+            
+            JsonSerializer serializer = new JsonSerializer();
 
+            using (StreamWriter streamWriter = new StreamWriter(path))
+
+            using (JsonWriter writer = new JsonTextWriter(streamWriter))
+            {
+                serializer.Serialize(writer, project);
+            }
+        }
     }
 }
